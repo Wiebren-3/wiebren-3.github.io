71b0ae71729f0352df3bcda8d11a9667bbec02f2
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2024-03-30 14:39:44 +0100

    Update code.js

--- code.js
+++ code.js
@@ -8,11 +8,11 @@
             switch (enteredCode) {
                 case "7888":
                     // Als de code "7888" is, stuur de gebruiker door naar "0513.html"
-                    window.location.href = "0513.html";
+                    window.location.href = "https://wiebren.space/0513/";
                     break;
                 case "999":
                     // Als de code "999" is, stuur de gebruiker door naar een externe pagina
-                    window.location.href = "https://www.links.com";
+                    window.location.href = "https://wiebren.space/links/";
                     break;
                 case "6393":
                     // Als de code "6393" is, stuur de gebruiker door naar een andere externe pagina
